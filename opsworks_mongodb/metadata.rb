b23877e40df608e0b930b6af8c376b2c7c9a465c
Pramod Singh <pramod.singh@kiwitech.com>
2016-12-21 17:28:47 +0530

    added opsworks_mongodb cookbook

---
+++ opsworks_mongodb/metadata.rb
@@ -0,0 +1,7 @@
+name        "opsworks_mongodb"
+description 'Cookbook for MongoDB Berkshelf based install'
+maintainer  "KiwiTech"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
